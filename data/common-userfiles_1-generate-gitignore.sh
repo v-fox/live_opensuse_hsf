178e45d2f50a9dda38a1b415bf6297c28f0e98a4
v-fox <virtuousfox@gmail.com>
2015-01-21 19:08:34 +0500

    enabling ALSA-to-JACK routing, making PA config non-system-wide, fixing gitignore generation

--- data/common-userfiles_1-generate-gitignore.sh
+++ data/common-userfiles_1-generate-gitignore.sh
@@ -5,7 +5,7 @@ DIRECTORY="common-userfiles"
 
 cd "${DIRECTORY}" || exit 1
 # generating empty files to force git not to ignore empty directories
-for i in $(find . -mindepth 1 -type d -empty -printf '%f\n'); do
+for i in $(find . -mindepth 1 -type d -empty | sed -e 's:^\./::g'); do
 	echo '!.gitignore' > "${i}/.gitignore"
 done
 
@@ -14,7 +14,7 @@ echo '!.gitignore' > "../../${GITIGNORE}"
 set -f              # turn off globbing
 IFS='
 '                   # split at newlines only
-for i in $(find . -mindepth 1 -type f ! \( -name '.gitignore' \) -printf '%h/%f\n' | sed -e 's:^\./::g' -e 's: :\\ :g'); do
+for i in $(find . -mindepth 1 -type f ! \( -name '.gitignore' \) | sed -e 's:^\./::g' -e 's: :\\ :g'); do
 	for f in etc/skel root home/hacker; do
 		echo "root/${f}/${i}" >> "../../${GITIGNORE}"
 	done
