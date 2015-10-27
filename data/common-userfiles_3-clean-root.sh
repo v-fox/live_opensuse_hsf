a806134b92612d790bf9efddc1edf069167b7795
v-fox <virtuousfox@gmail.com>
2015-10-27 20:02:35 +0500

    fix for broken clean up script and a link

--- data/common-userfiles_3-clean-root.sh
+++ data/common-userfiles_3-clean-root.sh
@@ -5,7 +5,7 @@ DIRECTORY="common-userfiles"
 
 cd "${DIRECTORY}" || exit 1
 # removing common files after building, just for tidiness
-for i in $(find . -mindepth 1 -printf '%f\n'); do
+for i in $(find . -mindepth 1); do
 	for f in etc/skel root home/hacker; do
 		if [ -e "${i}" ]; then
 			rm -rf "../../${FILE_OVERLAY}/${f}/${i}"
