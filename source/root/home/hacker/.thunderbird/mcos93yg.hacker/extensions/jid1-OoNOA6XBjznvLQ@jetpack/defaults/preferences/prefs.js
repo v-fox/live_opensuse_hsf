5a2e80bef1a3140a9f40e25eac2d248ee8706dae
v-fox <virtuousfox@gmail.com>
2015-12-25 08:32:37 +0500

    FF & TB profile update now with dark GTK theme workaround for FF

---
+++ source/root/home/hacker/.thunderbird/mcos93yg.hacker/extensions/jid1-OoNOA6XBjznvLQ@jetpack/defaults/preferences/prefs.js
@@ -0,0 +1,4 @@
+pref("extensions.jid1-OoNOA6XBjznvLQ@jetpack.downloadCompleteAlert", true);
+pref("extensions.jid1-OoNOA6XBjznvLQ@jetpack.clickOption", 1);
+pref("extensions.jid1-OoNOA6XBjznvLQ@jetpack.simpleNewMail", false);
+pref("extensions.jid1-OoNOA6XBjznvLQ@jetpack.notifyIcon", "default");
